0d4d33b18798d883432dd1f7c92d55853841cf69
shhur310 <seunghyehur@gmail.com>
2026-04-17 17:49:50 +0900

    0417 수정

--- worker/main.py
+++ worker/main.py
@@ -1,4 +1,8 @@
+import json
+import redis
 from llama_cpp import Llama
+
+redis_client = redis.from_url("redis://redis:6379", decode_responses=True)
 
 llm = Llama(
     model_path="./models/Llama-3.2-1B-Instruct-Q4_K_M.gguf",
@@ -15,15 +19,33 @@ SYSTEM_PROMPT = (
     "Do not mix language. "
 )
 
-user_input = "Python이 뭐야?"
+def run():
+    while True:
+        # 1) Queue에서 task를 dequeue
+        _, task = redis_client.brpop("queue")
 
-response = llm.create_chat_completion(
-    messages=[
-        {"role": "system", "content": SYSTEM_PROMPT},
-        {"role": "user", "content": user_input}
-    ],
-    max_tokens=256,
-    temperature=0.7
-)
+        task_data: dict = json.loads(task)
 
-print(response["choices"][0]["message"]["content"])
+        # 2) (반복)추론 -> 토큰 -> Publish
+        response_generator = llm.create_chat_completion(
+            messages=[
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": task_data["user_input"]},
+            ],
+            max_tokens=256,
+            temperature=0.7,
+            stream=True,
+        )
+
+        channel = task_data["channel"]
+        for chunk in response_generator:
+            token = chunk["choices"][0]["delta"].get("content")
+            if token:
+                redis_client.publish(channel, token)
+    
+        # 3) 추론 종료 알림: [Done] 메세지 전송
+        redis_client.publish(channel, "[DONE]")
+
+# 이 파일이 직접 실행한 경우에만, run() 호출
+if __name__ == "__main__":
+    run()
